37927507420543802397f102f0745ba5a29b4ba6
Bashidoshi Musa <musabashibawa@gmail.com>
2022-09-18 18:31:48 +0100

    Add files via upload

---
+++ Barter Trades/index.js
@@ -0,0 +1,9 @@
+/* Multi-line*/
+var myName= Musa
+console.log(myName)
+var a;
+var b = 2
+a =7
+b = a
+
+console.log(a)
---
+++ Barter Trades/trial.js
@@ -0,0 +1,9 @@
+//console.log('Hello world');
+//alert('Hello world');
+function difference(x, y){
+    z = x + y
+    return z
+}
+
+console.log(2, 3)
+
